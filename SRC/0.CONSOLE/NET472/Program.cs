a42d4199c5a9456350211f78959c3967a75409a8
jgavila <julio.avibas@gmail.com>
2024-05-28 22:04:59 -0500

    - CREA EJEMPLO CRUD EN PROYECTO CONSOLA > AGREGA REFERENCIA A LIBRERIAS PARA CONECTAR BD SQLITE > NET 4.7.2 (REPOSITORIO Y PRUEBAS)

--- SRC/0.CONSOLE/NET472/Program.cs
+++ SRC/0.CONSOLE/NET472/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,9 +60,46 @@ namespace NET472
                 Port = 3307
             };
 
+            var builderSqlite = new SQLiteConnectionStringBuilder
+            {
+                DataSource = "D:\\DESARROLLO\\PROYECTOS\\JGACODETEST\\VS2K22\\VS2K22\\SRC\\0.CONSOLE\\NET472\\Repository\\SQLite\\TESTSQLITEDB.sqlite",
+                Version = 3,
+                JournalMode = SQLiteJournalModeEnum.Wal,
+                Pooling = true
+            };
+            
+            //ListSQLServer(container, builderSQLServer);
 
+            //ListMySQL(container, builderMySQL);
 
+            //ListPostgres(container, builderPostgres);
 
+            //ListMariaDB(container, builderMariaDB);
+
+            using (DbConnection connection = new SQLiteConnection(builderSqlite.ConnectionString))
+            {
+                connection.Open();
+
+                var testTable1Repo = container.Resolve<ITestTable1Repo>(
+                    new ParameterOverride("existingConnection", connection),
+                    new ParameterOverride("useSchema", false)
+                );
+
+                Console.WriteLine("ITEMS SQLITE: ");
+
+                var testTable1List = testTable1Repo.getAll();
+
+                foreach (var item in testTable1List)
+                {
+                    Console.WriteLine("- " + item.Id.ToString() + " - " + item.Descripcion);
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void ListSQLServer(IUnityContainer container, SqlConnectionStringBuilder builderSQLServer)
+        {
             using (DbConnection connection = new SqlConnection(builderSQLServer.ConnectionString))
             {
                 connection.Open();
@@ -80,7 +118,10 @@ namespace NET472
                     Console.WriteLine("- " + item.Id.ToString() + " - " + item.Descripcion);
                 }
             }
+        }
 
+        private static void ListMySQL(IUnityContainer container, MySqlConnectionStringBuilder builderMySQL)
+        {
             using (DbConnection connection = new MySqlConnection(builderMySQL.ConnectionString))
             {
                 connection.Open();
@@ -99,7 +140,10 @@ namespace NET472
                     Console.WriteLine("- " + item.Id.ToString() + " - " + item.Descripcion);
                 }
             }
+        }
 
+        private static void ListPostgres(IUnityContainer container, NpgsqlConnectionStringBuilder builderPostgres)
+        {
             using (DbConnection connection = new NpgsqlConnection(builderPostgres.ConnectionString))
             {
                 connection.Open();
@@ -118,7 +162,10 @@ namespace NET472
                     Console.WriteLine("- " + item.Id.ToString() + " - " + item.Descripcion);
                 }
             }
+        }
 
+        private static void ListMariaDB(IUnityContainer container, MySqlConnectionStringBuilder builderMariaDB)
+        {
             using (DbConnection connection = new MySqlConnection(builderMariaDB.ConnectionString))
             {
                 connection.Open();
@@ -137,8 +184,7 @@ namespace NET472
                     Console.WriteLine("- " + item.Id.ToString() + " - " + item.Descripcion);
                 }
             }
-
-            Console.ReadLine();
         }
+
     }
 }
